ce3084dab76a5fda6b99651a691a10e8a06f6391
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-03-12 13:16:45 -0400

    fixed syntax errors to show locaiton

---
+++ zceirel/www/js/userTracking.js
@@ -0,0 +1,15 @@
+// code to check if location settings are on
+function trackLocation() {
+	if(navigator.geolocation) {
+		navigator.geolocation.watchPosition(showPosition);
+	} else {
+		document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser. Please change your settings and try again."
+	}
+}
+
+// code to display user location on map by drawing a point
+function showPosition(position) {
+	L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap)
+	.bindPopup("<b>You were here</b>");
+	
+}
