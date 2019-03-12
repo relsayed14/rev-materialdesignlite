fdee71056c7e9ec0490c1b023c896b8785ad9de6
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-03-12 12:55:43 -0400

    fixed errors to show poitn and line

--- zceirel/www/js/leafletFunctions.js
+++ zceirel/www/js/leafletFunctions.js
@@ -1,91 +1,65 @@
-	//global variables to be used by functions
-	var client;
-	var earthquakes;
+// code to get the Earthquakes data using an XMLHttpRequest
+var client;
+var earthquakes;
+var mymap;
 
 
-	//function to add a point, line and circle to the map
-	function addPointLinePoly() {
-		alert("Adding a point, line and circle to the map ...");
+function addPointLinePoly() {
+	//add a point
+	L.marker([50.93,-6.29]).addTo(mymap);
 
-		// add a point
-		L.marker([51.5,-0.09]).addTo(mymap)
-		.bindPopup("<b>Hello world!</b><br>I am a point.").openPopup();
+	//add a circle
+	L.circle([51.5,-0.09],50000, {
+		color:'red',
+		fillColor:'#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap);
 
-		//add a circle
-		L.circle([51.508,-0.11],500, {
+
+	//add a polygon with 2 end points (i.e. a line)
+	var myLine = L.polygon([
+		[55.7,-21.81],
+		[58.51,13.32]
+		],{
+
 			color:'red',
-			fillColor:'#f03',
-			fillOpacity:0.5
-		}).addTo(mymap).bindPopup("I am a circle.");
+			fillColor:'#f06',
+			fillOpacity: '0.5'
 
-		// add a line
-		var myLine = [
-		[51.5, -0.07],
-		[51.51, -0.08]
-		];
-		L.polyline(myLine,{color:'green'})
-		.addTo(mymap).bindPopup("I am a line.");
-	}
+		}).addTo(mymap);
 
-	// code to get the Earthquakes data using an XMLHttpRequest
-	function getEarthquakes() {
-		client = new XMLHttpRequest();
-		client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'); 
-			//if open request has been invoked successfully, call the earthquakeResponse function
-			client.onreadystatechange = earthquakeResponse; 
-			// note don't use earthquakeResponse() with brackets as that doesn't work
-			client.send(); 
-		}
+}
 
-		// code to wait for the response from the data server, and process the response once it is received
-		function earthquakeResponse() {
-		// this function listens out for the server to say that the data is ready - i.e. has state 4 
-		if (client.readyState == 4) {
-			// once the data is ready, process the data 
-			var earthquakedata = client.responseText; 
-			loadEarthquakelayer(earthquakedata);
-		}
-	}
+// Retrieve the Earthquake data layer 
+function getEarthquakes() {
+	client = new XMLHttpRequest();
+	client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
+	client.onreadystatechange = earthquakeResponse;  
+	client.send();
+}
 
 
+// Wait for the response from data server
+// process the response once received
+function earthquakeResponse() {
+  // this function listens out for the server to say that the data is ready - i.e. has state 4
+  if (client.readyState == 4) {
+    // once the data is ready, process the data
+    var earthquakedata = client.responseText;
+    loadGeoJSONLayer(earthquakedata);
+}
+}
 
-	// convert the received data - which is text - to JSON format and add it to the map 
-	function loadEarthquakelayer(earthquakedata) {
-		// convert the text to JSON
-		var earthquakejson = JSON.parse(earthquakedata);
+// convert the received data (text) to JSON format 
+// add it to the map
+function loadGeoJSONLayer(earthquakedata) {
+    // convert the text to JSON
+    var earthquakejson = JSON.parse(earthquakedata);
+    earthquakes = earthquakejson;
 
-		// loan the JSON layer onto the map
-		earthquakelayer = L.geoJson(earthquakejson, {
-		//use point to layer to create the points
-		pointToLayer: function(feature,latlng)
-		{
-					//look at the GeoJSON file - specifically at the properties,to see the earthquake magnitude and use a different marker depending on this value
-					// also incldue a pop up that shows the place value of the earthquakes
-					if(feature.properties.mag > 1.75) {
-						return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+ feature.properties.place + "</b>");
-					} else {
-						// if mag =< 1.75
-						return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+ feature.properties.place + "</b>");
+    // add the JSON layer onto the map - it will appear using the default icons
+    geojsonLayer = L.geoJson(earthquakejson).addTo(mymap);
 
-					}
-				},
-			}).addTo(mymap);
-
-	// change the map zoom so that all the data is shown
-	mymap.fitBounds(earthquakelayer.getBounds()); 
-
-// create red test marker
-var testMarkerRed = L.AwesomeMarkers.icon({
-	icon: 'play',
-	markerColor: 'red'
-});
-
-
-// create pink test marker
-var testMarkerPink = L.AwesomeMarkers.icon({
-	icon: 'play',
-	markerColor: 'pink'
-});
-
-
-
+    // change the map zoom so that all the data is shown
+    mymap.fitBounds(geojsonLayer.getBounds());
+}
